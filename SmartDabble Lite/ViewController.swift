2382f3196a2eda89ad2fa94b1dc00cedcf21825a
Mamajonov Elbek <mamajonovelbek@mamajonovs-macbook-pro.local>
2018-10-25 00:45:22 +0500

    another print func is added

--- SmartDabble Lite/ViewController.swift
+++ SmartDabble Lite/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("lets get started!")
+        print("done in the branch")
     }
 
    
