51a792f8198e431b944a1eb4eedec09c0ff7e810
Mamajonov Elbek <mamajonovelbek@mamajonovs-macbook-pro.local>
2018-10-26 00:52:58 +0500

    Added icons to "Assets.xcassets" folder and set to appropriate BarItems

---
+++ SmartDabble Lite/RunViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RunViewController.swift
+//  SmartDabble Lite
+//
+//  Created by Mamajonov Elbek on 10/25/18.
+//  Copyright © 2018 Mamajonov Elbek. All rights reserved.
+//
+
+import UIKit
+
+class RunViewController: UIViewController {
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
